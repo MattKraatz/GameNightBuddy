f3710dd32202822d1042e11ffabc0cc93fe830d3
Matt Kraatz <matt.kraatz@gmail.com>
2017-01-24 11:23:25 -0600

    handle multiple input fields for new collection game

--- client/src/components/Collection.jsx
+++ client/src/components/Collection.jsx
@@ -2,38 +2,58 @@ import React from 'react';
 import ReactFireMixin from 'reactfire';
 import Firebase from 'firebase';
 
+const emptyInput = {newGame: {
+  name: '',
+  minPlayers: '',
+  maxPlayers: '',
+  owner: ''
+}}
+
 export default React.createClass({
   mixins: [ReactFireMixin],
   getInitialState: function() {
-    return {text: ""};
+    return emptyInput;
   },
   componentWillMount: function() {
     var ref = Firebase.database().ref('games');
     this.bindAsArray(ref, 'games');
   },
   onChange: function(e) {
-    this.setState({text: e.target.value});
+    const target = e.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    const game = this.state.newGame;
+    game[name] = value;
+    this.setState(game);
   },
   handleSubmit: function(e) {
     e.preventDefault();
-    if (this.state.text && this.state.text.trim().length !== 0) {
-      this.firebaseRefs['games'].push({
-        name: this.state.text
-      });
-      this.setState({
-        text: ''
-      });
+    if (this.state.newGame.name && this.state.newGame.name.trim().length !== 0) {
+      this.firebaseRefs['games'].push(this.state.newGame);
+      this.setState(emptyInput);
     }
   },
   render: function() {
     return <div className="collection">
+      <h2>Games Available</h2>
       {this.state.games.map((item, index) => {
         return <div key={item[".key"]}>
             <div>{item.name}</div>
           </div>
       })}
+      <h2>New Game</h2>
       <form onSubmit={ this.handleSubmit }>
-        <input onChange={ this.onChange } value={ this.state.text } />
+        <label htmlFor="name">Name</label>
+          <input name="name" value={ this.state.newGame.name } onChange={ this.onChange } />
+        <br />
+        <label htmlFor="owner">Owner</label>
+          <input name="owner" value={ this.state.newGame.owner } onChange={ this.onChange } />
+        <br />
+        <label htmlFor="minPlayers">Minimum # of Players</label>
+          <input name="minPlayers" value={ this.state.newGame.minPlayers } onChange={ this.onChange } />
+        <br />
+        <label htmlFor="maxPlayers">Maximum # of Players</label>
+          <input name="maxPlayers" value={ this.state.newGame.maxPlayers } onChange={ this.onChange } />
         <button>{ 'Add #' + (this.state.games.length + 1) }</button>
       </form>
     </div>
