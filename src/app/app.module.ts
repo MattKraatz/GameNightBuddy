f8d81189951ca8ef73c4d30db764415d09c0d798
Matt Kraatz <matt.kraatz@gmail.com>
2017-03-26 18:14:17 -0500

    basic app structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MatchesComponent } from './matches/matches/matches.component';
+import { CollectionComponent } from './collection/collection/collection.component';
+import { LoginComponent } from './auth/login/login.component';
+import { MembersComponent } from './members/members/members.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatchesComponent,
+    CollectionComponent,
+    LoginComponent,
+    MembersComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
