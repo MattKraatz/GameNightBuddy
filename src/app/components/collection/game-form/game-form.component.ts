98c6959e94eec05ef26a94ba0b44a922d656126c
Matt Kraatz <matt.kraatz@gmail.com>
2017-04-02 10:25:54 -0500

    fixed select input bugs

--- src/app/components/collection/game-form/game-form.component.ts
+++ src/app/components/collection/game-form/game-form.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import {Game} from '../../../models/game.model';
 import {Member} from '../../../models/member.model';
 import {CollectionService} from '../../../services/collection.service';
+import {MembersService} from '../../../services/members.service';
 
 @Component({
   selector: 'app-game-form',
@@ -12,7 +13,7 @@ export class GameFormComponent implements OnInit {
 
   @Input() members: Member[];
 
-  constructor(private collectionService: CollectionService) { }
+  constructor(private collectionService: CollectionService, private membersService: MembersService) { }
 
   ngOnInit() {
   }
