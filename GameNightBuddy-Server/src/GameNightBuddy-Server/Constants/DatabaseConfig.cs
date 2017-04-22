1df10a2bbc8d85dfa69cb05fd559fe771c3cce0f
Matt Kraatz <matt.kraatz@gmail.com>
2017-04-22 17:48:20 -0500

    ef setup

---
+++ GameNightBuddy-Server/src/GameNightBuddy-Server/Constants/DatabaseConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameNightBuddy_Server.Constants
+{
+    public static class DatabaseConfig
+    {
+      public const string ConnectionString = "";
+    }
+}
