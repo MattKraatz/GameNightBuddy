09341d24f86a71d4b0a7e29dae027a71e810568c
Matt Kraatz <matt.kraatz@gmail.com>
2017-06-05 07:56:57 -0500

    build for release

--- src/app/components/explore/explore.component.ts
+++ src/app/components/explore/explore.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 
 import {GameNight} from '../../models/game-night.model';
