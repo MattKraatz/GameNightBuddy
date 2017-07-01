a447d2d359c487a4fe5d6c4ad48828b0863c88ec
Matt Kraatz <matt.kraatz@gmail.com>
2017-07-01 11:27:33 -0500

    match form player select working better

--- src/app/components/matches/match-form/match-form.component.ts
+++ src/app/components/matches/match-form/match-form.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import {Game} from '../../../models/game.model';
 import {Member} from '../../../models/member.model';
 import {Match} from '../../../models/match.model';
+import {Player} from '../../../models/player.model';
 import {MatchService} from '../../../services/match.service';
 
 @Component({
@@ -17,14 +18,33 @@ export class MatchFormComponent implements OnInit {
   @Output() addMatch = new EventEmitter();
 
   model: Match;
+  playersModel: Player[];
 
   constructor(private matchService: MatchService) {
     this.model = new Match();
+    this.playersModel = new Array<Player>();
   }
 
   ngOnInit() {
     if (this.match) {
       this.model = this.match;
+      this.match.Players.forEach((p) => this.playersModel.push(p));
+    }
+  }
+
+  // manually control state between the player select element and the players table
+  playerSelectOnChange(players: Player[]){
+    if (players && players.length) {
+      // remove players
+      this.model.Players = this.model.Players.filter(p => {
+        return 0 <= players.findIndex(pl => pl.MemberId === p.MemberId);
+      })
+      // add players
+      players.forEach(p => {
+        if (-1 === this.model.Players.findIndex(mp => mp.MemberId === p.MemberId)) {
+          this.model.Players.push(p);
+        }
+      })
     }
   }
 
