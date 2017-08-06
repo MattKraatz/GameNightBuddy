bed62153886982571475d06fe8e86b77222129fe
Matt Kraatz <matt.kraatz@gmail.com>
2017-08-06 10:04:06 -0500

    styling, bug fixes

--- src/app/services/resolvers/game-night-resolver.service.ts
+++ src/app/services/resolvers/game-night-resolver.service.ts
@@ -12,12 +12,9 @@ export class GameNightResolver implements Resolve<GameNight> {
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<GameNight> {
     let id = route.params['id'];
-    let loaded: boolean = this.gameNightService.nightLoaded;
+    // let loaded: boolean = this.gameNightService.nightLoaded;
     this.gameNightService.loadGameNight(id);
     // Complete the Observable by calling .first
-    // If the Game Night was already loaded, take the first value, else take the second
-    return loaded ?
-      this.gameNightService.gameNight.first() :
-      this.gameNightService.gameNight.skip(1).first();
+    return this.gameNightService.gameNight.skip(1).first();
   }
 }
